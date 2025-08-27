bfcf047025061598b9b5a94e239539063c793c9a
shfuerte79-web <shfuerte79@gmail.com>
2025-08-27 09:05:15 +0100

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-24">
+      <h1 className="text-4xl font-bold text-center">
+        Ideas from: Voice-to-Code Generator
+      </h1>
+    </main>
+  );
+}
